cbc857ed9324bb0e67bff1376402740e3825b1ad
Ekaterina <kate@MacBook-Air-Ekaterina.local>
2019-12-22 20:44:33 +0300

    v5

--- project/my_store/my_store/settings.py
+++ project/my_store/my_store/settings.py
@@ -121,8 +121,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = os.path.join(BASE_DIR, '/media/')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+print('BASE_DIR', BASE_DIR)
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
